d2713f394fe1875eaec2f7d2b6671edb50014274
Gabriel Vega <gvega001@gmail.com>
2021-03-10 21:26:04 -0800

    Card Class Created

---
+++ Application/GamesLibrary/Card.cs
@@ -0,0 +1,17 @@
+﻿namespace GamesLibrary
+{
+    public class Card
+    {
+        private string Face { get; } //Card's face 
+        private string Suit { get; } // Card's Suit
+
+        public Card(string face, string suit)
+        {
+            Face = face; //initial face of card
+            Suit = suit; // initial suit of card
+        }
+
+        // return string representation of card
+        public override string ToString() => $"{Face} of {Suit}";
+    }
+}
